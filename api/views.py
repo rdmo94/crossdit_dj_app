992ca88cb306e288b2b34d0e74833da56a307caa
rdmo <rdmo@itu.dk>
2020-11-12 20:54:19 +0100

    ?

--- api/views.py
+++ api/views.py
@@ -13,7 +13,7 @@ def welcome(request):
 
 @api_view(["GET"])
 def getReading(request):
-    client = MongoClient('mongodb+srv://crossdit:crossdit@cluster0.yqnrt.mongodb.net/crossdit?retryWrites=true&w=majority', connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True, connect=False, maxPoolsize=1)
+    client = MongoClient('mongodb+srv://crossdit:crossdit@cluster0.yqnrt.mongodb.net/crossdit?retryWrites=true&w=majority&authSource=admin', connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True, connect=False, maxPoolsize=1)
     db = client.crossdit
     collection = db.data
     result = collection.aggregate([{ "$sample" : { "size" : 1 } }])
